148e55682feb2ada199990e012957d9245909fa6
Paul Webanck <paul.webanck@edhec.com>
2017-10-03 17:32:37 +0200

    Style improved

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,6 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :photo, presence: true
+  mount_uploader :photo, PhotoUploader
 end
